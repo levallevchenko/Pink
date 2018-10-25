f1bbe388abad7b47b89a6e4ccac11b17e3da53ab
Анна Левченко <levallevchenko@gmail.com>
2018-10-25 23:16:05 +0300

    Начата работа над векторной графикой

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var gulp = require("gulp");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,6 +22,23 @@ gulp.task("css", function () {
     ]))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("sprite", function () {
+    return gulp.src("source/img/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("server", function () {
